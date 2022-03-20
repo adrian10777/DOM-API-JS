4fc707a9f287945e78a2ac578bced53e0e452cfb
Adrian Henriquez <adrianhenriquez@Adrians-MacBook-Pro.local>
2022-03-20 16:44:20 -0400

    secondcommit

---
+++ static/JS/APIS.js
@@ -0,0 +1,72 @@
+/*
+JavaScript and APIs (Axios)
+How this project will function:
+1. User submits a form (JS event listener)
+2. Form data is brought in and saved (process associated with the event listener)
+3. If the form is being resubmitted, display welcome back (you'll see what I mean by this as we get into things) (process associated with the event listener)
+4. Query an API with an Axios get request for a CatFact (Axios!)
+5. Ignore whether or not the user wants a CatFact (you'll se what I mean by this later) (form related)
+6. Display the CatFact for the user in our HTML DOM (creation of HTML elements using JS)
+*/
+
+// access to our form
+let form = document.querySelector('#catform');
+console.log(form);
+// some place to store the user data
+// plan is for the keys to be user's names (who submitted the form), value will be an array of their CatFacts
+let users = {};
+
+// listen for submit event
+form.addEventListener('submit', (event) => {
+    // stop the page from reloading on form submission
+    event.preventDefault();
+
+    // get the form data - 2 options: event.path or querySelector
+    // why event.path? we already have access to the event.path - no reason to go re-get the data thru querySelector
+    let name = event.path[0][0].value;
+    let wishes = event.path[0][1].value;
+    
+    // use that data - save the user's name in our users object if not already present
+    if (users[name]){
+        // user exists - display a welcome back message in html
+        document.querySelector('#welcome-message').innerHTML = `Welcome back, ${name}!`
+    } else {
+        // display new user welcome message
+        // and add the user to users object
+        users[name] = [];
+        document.querySelector('#welcome-message').innerHTML = `Welcome to CatFacts, ${name}!`
+    }
+
+    // build a little 3 part conditional to use the wishes portion of the form data
+    wishes.toLowerCase().includes('yes') ? alert('Thank you for subscribing to CatFacts! Press ok to receive your CatFact!') :
+    wishes.toLowerCase().includes('no') ? alert("It's a real shame that you don't want a CatFact. Too bad. You're getting one anyway.") :
+    alert("I couldn't tell if you wanted a CatFact or not. So anyway, here's a CatFact.");
+    
+    // make the API call happen - call the API process function(s)
+    loadData(name);
+
+    // reset the form after we're done
+    form.reset();
+})
+
+// making the APIcall happen: Axios
+let getData = async () => {
+    // api call
+    let response = await axios.get('https://catfact.ninja/fact');
+    return response.data.fact
+}
+
+// an async process must remain async -> do stuff with our fact
+// does stuff with the data from the api call - aka makes an html
+let loadData = async (user) => {
+    let catFact = await getData();
+    console.log(catFact); // we have access to the actual string - we can make an html element to be added to the page
+    // add the catFact to the user's array
+    users[user].push(catFact);
+    console.log(users); // take a peek at the users object
+    // create the html element for the catFact
+    let html = document.createElement('h3');
+    html.className = 'newfact';
+    html.innerHTML = catFact;
+    document.querySelector('.fact').appendChild(html);
+}
--- static/JS/dom.js
+++ static/JS/dom.js
@@ -1,105 +1,101 @@
-/*
-JS AND THE HTML DOM
+console.log('Hello, World.');
 
-we can use JS to directly access and modify the HTML DOM, attributes, classes, tags, w.e
-we do that through the document object
+/*
+JavaScript and the HTML DOM
+We can use JS to directly access and modify the HTML DOM, HTML attributes, classses, tags, whatever
+We do that through the document object
 */
 
-// accessing element by tag name
+// accessing an element by tag name
 let main_header = document.getElementsByTagName('h1');
 console.log(main_header);
 console.log(main_header[0]);
-console.log(main_header[0].className);
 console.log(main_header[0].innerHTML);
 console.log(typeof main_header[0].innerHTML);
-// main_header[0].className = 'page-title'; // modifying classname of an element
 main_header[0].innerHTML = main_header[0].innerHTML.toUpperCase(); // modifying the innerhtml of an element
 
-//accessing element by id
-let main_header_access2 = document.getElementById('header');
-console.log(main_header_access2);
-main_header.innerHTML= 'SharkFact:'; // further modifying the innerhtml of an element
+// accessing an element by ID
+let main_header_access_2 = document.getElementById('header');
+console.log(main_header_access_2);
+main_header_access_2.innerHTML = 'SharkFact:'; // further modifying the innerhtml of an element
+
 
 // accessing our button
-
 let color_button = document.getElementById('color-button');
 console.log(color_button);
 
-// lets come up with process for hwen button is pressed
-
+// let's come up with a process for when the button is pressed
 let color_change = () => {
     let e = document.getElementById('header');
     if (e.className == '') {
         e.className = 'color-change';
     } else if (e.className == 'color-change') {
-        e.className = 'color-change-two'
+        e.className = 'color-change-two';
     } else {
         e.className = '';
     }
 }
 
-// how do we get the button press to run this process
-// rememeber that we talked about JS being an event based language
-// we need to tell our JS to listen for the event of the button press
-// we do that through Eventlisteners!
+// how do we get the button press to run this process?
+// remember that we talked about JS being an event-based language
+// we need to tell our JS to 'listen' for the event of the button press
+// we do that through EventListeners!
 
 // add a click eventlistener to the color_button to run color_change when clicked
-color_button.addEventListener('click', color_change)
+color_button.addEventListener('click', color_change);
 
-// create HTML element through my JS
-let new_button = document.createElement('Element');
+// create an html element through my javascript
+let new_button = document.createElement('button');
 let to_be_displayed = document.createElement('p');
 
-//to_be_displayed.className = 'color-change';
+// add some text to the button
+new_button.innerHTML = 'Click for a SharkFact.'
 
-// add some text to button
-new_button.innerHTML = 'Click for more SharkFacts';
-
-// add a className and hide to_be_displayed
-
-to_be_displayed = 'color-change';
+//add a className and hide to_be_displayed
+to_be_displayed.className = 'color-change';
 to_be_displayed.hidden = true;
 
-// add a new button and p tag to the pg
+// add the new button and p tag to the page
 document.body.append(new_button);
 document.body.append(to_be_displayed);
 
-// process for what happens when new button is pressed
+// create a process for what happens when the new button is clicked
 let sharkfacts = () => {
     if (to_be_displayed.hidden){
-        to_be_displayed.innerHTML = 'Most greenLand sharks are blind';
-        new_button.innerHTML = 'Hide Sharkfact';
+        to_be_displayed.innerHTML = 'Most Greenland sharks are blind. Due to their slow swimming speed, their eyes are succeptible to a specific species of parasite.';
+        new_button.innerHTML = 'Hide SharkFact';
         to_be_displayed.hidden = false;
     } else {
-        new_button.innerHTML = 'click for a sharkfact'
-        to_be_displayed.hidden= true;
+        new_button.innerHTML = 'Click for a SharkFact.';
+        to_be_displayed.hidden = true;
     }
 }
 
-// event listener
+// event listener for new button
 new_button.addEventListener('click', sharkfacts);
 
-/* interacting with an HTML form */
-let sharkfactform = document.getElementById('sharkform');
+/* Interacting with an HTML Form */
+
+// access my form
+let sharkfactform = document.getElementById('sharkForm');
 console.log(sharkfactform);
 
-// lets have JS control what happens when form is submitted
+// let's have our JavaScript control what happens when the form is submitted
 // event listener for submit!
 
 sharkfactform.addEventListener('submit', (event) => {
     event.preventDefault();
     console.log(event);
-    // 2 ways to get form data
-    // fav way - through event path
+    // 2 ways to get the form data
+    // my favorite way - through the event.path
     let sharkname = event.path[0][0].value;
     let sharkfact = event.path[0][1].value;
     console.log(`Event path data: ${sharkname} | ${sharkfact}`);
-    // OTHER WAY to get form data - query selector
-    let qsharkname = document.querySelector('shark-name').value
-    letqsharkfact = document.querySelector('shark-fact').value
-    console.log(` query selector data: ${qsharkname} | ${qsharkfact}`);
+    // the other way to get form data - through the query selector
+    let qsharkname = document.querySelector('#shark-name').value;
+    let qsharkfact = document.querySelector('#shark-fact').value;
+    console.log(`Query selector data: ${qsharkname} | ${qsharkfact}`);
 
-    //reset form
+    // reset form
     sharkfactform.reset();
-})
-
+});
